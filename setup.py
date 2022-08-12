c508b4c0c714c33b13b4fa59f72c0ab911ec4f46
Piatik <54076033+Piatik@users.noreply.github.com>
2022-08-12 13:43:15 +0200

    Add ping command

--- setup.py
+++ setup.py
@@ -3,12 +3,15 @@ import discord
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
+prefix = &
+
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        if message.startswith(prefix + "ping"):
+            message.reply("Pong")
 
 client = MyClient()
 client.run(TOKEN)
