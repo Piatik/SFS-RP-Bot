528f9f4216421eb38eafa59febd2e96134e78c7c
Piatik <54076033+Piatik@users.noreply.github.com>
2022-08-12 16:40:16 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import discord
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
 prefix = "&"
+money=20
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -13,7 +14,7 @@ class MyClient(discord.Client):
         if message.content.startswith(prefix + "ping"):
             await message.reply("Pong")
         if message.content.startswith(prefix + "money"):
-            embed=discord.Embed(title=name, color=0x00ffff)
+            embed=discord.Embed(title=author.name, color=0x00ffff)
             embed.add_field(name="Money" , value=money, inline=True)
             await message.send(embed=embed)
 
