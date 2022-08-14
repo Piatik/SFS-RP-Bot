13eac0402a078829fcc09f67e5316ef2826998ab
Piatik <54076033+Piatik@users.noreply.github.com>
2022-08-14 23:58:57 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,11 +1,12 @@
 import os
 import discord
+from discord.ext import commands
 import Cogs.Missions.Planete as Planete
 from Cogs.Missions.Mission import Mission
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
-bot = discord.ext.commands.Bot(debug_guilds=[988710399685840926])
+bot = commands.Bot(debug_guilds=[988710399685840926])
 
 bot.load_extension('Cogs.Missions.CogMission')
 
