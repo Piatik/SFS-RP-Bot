a4d4fe72a5d956a9936011ff3a6f74f578e46292
Piatik <54076033+Piatik@users.noreply.github.com>
2022-08-15 13:35:31 +0200

    Update CogMission.py

--- Cogs/Missions/CogMission.py
+++ Cogs/Missions/CogMission.py
@@ -1,6 +1,6 @@
 #from turtle import title
 import discord
-#from discord.ext import commands
+from discord.ext import commands
 from discord.ui import Button , View
 #from interactions import Modal
 #from Cogs.Missions.Mission import Mission
