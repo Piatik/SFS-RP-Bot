2934fe846b111f2f2e30e2f06c095a57d026d66e
Piatik <54076033+Piatik@users.noreply.github.com>
2022-08-16 19:11:03 +0200

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -7,19 +7,9 @@ TOKEN = os.environ.get('DISCORD_TOKEN')
 
 bot = discord.Bot(debug_guilds=[988710399685840926])
 
-bot.load_extension('Cogs.Missions.CogMission')
+extensions = ['Cogs.Missions.CogMission','Cogs.CogHelp']
+for cog in extensions:
+    bot.load_extension(cog)
 
-
-@discord.slash_command(
-    name="help",
-    description="Affichage des commandes"
-)
-def help(ctx):
-    embed = discord.Embed(color=0x00ff00)
-    embed.set_thumbnail(url="https://www.crushpixel.com/big-static14/preview4/planet-space-with-stars-shiny-1674010.jpg")
-    embed.add_field(name="/mission", value="Permets de calculer vos couts de mission ainsi que vos bénefices facilement !")
-    embed.add_field(name="/satellite", value="Affiche les satellite de la planete spécifier ainsi que leur couts")
-    embed.add_field(name="/planete",value="Donne la table des couts pour les objectif de mission pour la planete spécifié")
-    ctx.send(embed=embed)
 
 bot.run(TOKEN)
