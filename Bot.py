cd65da1fd22163394de14a24d72c1489eff06eaa
Maxime <maximemerveille59@gmail.com>
2022-08-28 20:26:25 +0200

    version mise a jour

--- Bot.py
+++ Bot.py
@@ -1,9 +1,9 @@
-import os
+from asyncio.windows_events import NULL
 import discord
 import Cogs.Missions.Planete as Planete
 from Cogs.Missions.Mission import Mission
 
-TOKEN = os.environ.get('DISCORD_TOKEN')
+
 
 bot = discord.Bot(debug_guilds=[988710399685840926])
 
@@ -11,8 +11,5 @@ extensions = ['Cogs.Missions.CogMission','Cogs.CogHelp']
 for cog in extensions:
     bot.load_extension(cog)
 
-@bot.event
-async def on_ready():
-    print('Logged on as {0}!'.format(bot.user))
 
-bot.run(TOKEN)
+bot.run('OTg4ODEwOTQwMjEwMzgwODMw.G0svH6.o8By9XvqCP9RaK-673K-R96Zn77PXIuY0goRTI')
--- Cogs/CogHelp.py
+++ Cogs/CogHelp.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import Option
 
 
 
@@ -8,18 +9,23 @@ class CogHelp(commands.Cog):
     
     def __init__(self, bot):
         self.bot = bot
+       
+
+    
 
     @discord.slash_command(
         name="help",
         description="Affichage des commandes"
     )
     async def help(self,ctx):
-        embed = discord.Embed(color=0x00ff00)
+       
+        embed = discord.Embed(color=0x00ff00, title="Page d'aide sur les commandes")
         embed.set_thumbnail(url="https://www.crushpixel.com/big-static14/preview4/planet-space-with-stars-shiny-1674010.jpg")
-        embed.add_field(name="/mission", value="Permets de calculer vos couts de mission ainsi que vos bénefices facilement !",inline = False)
-        embed.add_field(name="/satellite", value="Affiche les satellite de la planete spécifier ainsi que leur couts",inline = False)
-        embed.add_field(name="/planete",value="Donne la table des couts pour les objectif de mission pour la planete spécifié",inline = False)
-        await ctx.send(embed=embed)
+        embed.add_field(name="`/mission <planete ou satellite>`", value="Permets de calculer vos couts de mission vers une planete ou un satellite ainsi que vos bénefices facilement via un questionnaire.",inline = False)
+        embed.add_field(name="`/satellite <planete>`", value="Affiche les satellite de la planete spécifié ainsi que leur couts de mission.",inline = False)
+        embed.add_field(name="`/prix <planete>`",value="Donne la table des couts pour le type et les objectif de mission pour la planete spécifié.",inline = False)
+        await ctx.respond(embed=embed)
+
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(CogHelp(bot))
