142550670287d60c2570f7b296852c2215b59f12
Piatik <54076033+Piatik@users.noreply.github.com>
2022-08-28 22:33:18 +0200

    Token Discretion

--- Bot.py
+++ Bot.py
@@ -3,7 +3,7 @@ import discord
 import Cogs.Missions.Planete as Planete
 from Cogs.Missions.Mission import Mission
 
-
+TOKEN = os.environ.get('DISCORD_TOKEN')
 
 bot = discord.Bot(debug_guilds=[988710399685840926])
 
@@ -11,5 +11,8 @@ extensions = ['Cogs.Missions.CogMission','Cogs.CogHelp']
 for cog in extensions:
     bot.load_extension(cog)
 
+@bot.event
+async def on_ready():
+    print('Logged on as {0}!'.format(bot.user))
 
-bot.run('OTg4ODEwOTQwMjEwMzgwODMw.G0svH6.o8By9XvqCP9RaK-673K-R96Zn77PXIuY0goRTI')
+bot.run(TOKEN)
