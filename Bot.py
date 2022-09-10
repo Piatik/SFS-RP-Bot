967ef53ed7de9b4b598900b064eef41027f7d927
Piatik <54076033+Piatik@users.noreply.github.com>
2022-09-10 16:36:58 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -7,12 +7,11 @@ TOKEN = os.environ.get('DISCORD_TOKEN')
 
 bot = discord.Bot(debug_guilds=[988710399685840926])
 
-extensions = ['Cogs.Missions.CogMission','Cogs.CogHelp']
-for cog in extensions:
-    bot.load_extension(cog)
-
 @bot.event
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
 
+    
+bot.load_extension('Cogs.Missions.CogMission')
+bot.load_extension('Cogs.CogHelp')
 bot.run(TOKEN)
