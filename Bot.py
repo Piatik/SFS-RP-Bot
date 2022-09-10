6ab0e4af7745241b147e81aef332b1bf4a445b9a
Piatik <54076033+Piatik@users.noreply.github.com>
2022-09-10 16:55:37 +0200

    Cleaning

--- Bot.py
+++ Bot.py
@@ -7,12 +7,12 @@ TOKEN = os.environ.get('DISCORD_TOKEN')
 
 bot = discord.Bot(debug_guilds=[988710399685840926])
 
+extensions = ['Cogs.Other.CogMission','Cogs.CogHelp']
+for cog in extensions:
+    bot.load_extension(cog)
+
 @bot.event
 async def on_ready():
     print('Logged on as {0}!'.format(bot.user))
 
-print("Start CogMission Loading")
-bot.load_extension('Cogs.Other.CogMission')
-print("End CogMission Loading")
-bot.load_extension('Cogs.CogHelp')
 bot.run(TOKEN)
