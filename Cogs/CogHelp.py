368f43177c2026f4424761a8a1541d314c7fcd8c
Piatik <54076033+Piatik@users.noreply.github.com>
2022-09-11 18:18:40 +0200

    Update CogHelp.py

--- Cogs/CogHelp.py
+++ Cogs/CogHelp.py
@@ -21,9 +21,9 @@ class CogHelp(commands.Cog):
        
         embed = discord.Embed(color=0x00ff00, title="Page d'aide sur les commandes")
         embed.set_thumbnail(url="https://www.crushpixel.com/big-static14/preview4/planet-space-with-stars-shiny-1674010.jpg")
-        embed.add_field(name="`/mission <planete ou satellite>`", value="Permets de calculer vos couts de mission vers une planete ou un satellite ainsi que vos bénefices facilement via un questionnaire.",inline = False)
-        embed.add_field(name="`/satellite <planete>`", value="Affiche les satellite de la planete spécifié ainsi que leur couts de mission.",inline = False)
-        embed.add_field(name="`/prix <planete>`",value="Donne la table des couts pour le type et les objectif de mission pour la planete spécifié.",inline = False)
+        embed.add_field(name="**`/mission <planete ou satellite>`**", value="Permets de calculer vos couts de mission vers une planete ou un satellite ainsi que vos bénefices facilement via un questionnaire.",inline = False)
+        embed.add_field(name="**`/satellite <planete>`**", value="Affiche les satellite de la planete spécifié ainsi que leur couts de mission.",inline = False)
+        embed.add_field(name="**`/prix <planete>`**",value="Donne la table des couts pour le type et les objectif de mission pour la planete spécifié.",inline = False)
         await ctx.respond(embed=embed)
 
 
