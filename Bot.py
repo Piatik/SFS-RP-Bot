6761fe07abbc273628ec52ad293efbb0de23290d
Piatik <54076033+Piatik@users.noreply.github.com>
2023-01-01 00:58:28 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,7 +5,7 @@ from Cogs.Other.Mission import Mission
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
 
-bot = discord.Bot(debug_guilds=[1007604816530374727], [827924843944738817])
+bot = discord.Bot(debug_guilds=[827924843944738817])
 
 extensions = ['Cogs.Other.CogMission','Cogs.CogHelp']
 for cog in extensions:
